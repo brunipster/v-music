240631c26802b8d8eba0f9b3210cf52208291632
Bruno Reyes <brunorebu12@gmail.com>
2020-05-02 19:07:26 -0500

    first commit

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 📻  V Music
+            .nav-right.nav-menu
+              router-link.nav-item(:to="{ name: 'search' }") Buscar
+              router-link.nav-item(to="about") Nosotros
+
+    .hero-body
+      .container.has-text-centered
+        h1.title V Music
+        h2.subtitle Canciones que estan Vue-nisimas!
+        pm-player
+</template>
+
+<script>
+import PmPlayer from "@/components/Player.vue";
+
+export default {
+  components: { PmPlayer }
+};
+</script>
+<style lang="scss" scoped>
+</style>
